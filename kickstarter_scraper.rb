da0fb24903c644f78e5f5421dac9fb8df69a5a7f
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-13 08:35:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -24,9 +24,9 @@ require 'pry'
   def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
- 
+
   projects = {}
- 
+
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
@@ -36,9 +36,7 @@ require 'pry'
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
- 
-  
+
+
   projects
 end
-
-
